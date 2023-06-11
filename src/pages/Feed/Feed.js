134e7383f3c6c1f83d009022440c62358acb9daa
adamanandas <adamananda02@gmail.com>
2023-06-11 21:14:29 +0700

    Fixing pagination bug

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -273,12 +273,14 @@ class Feed extends Component {
         };
         this.setState((prevState) => {
           let updatedPosts = [...prevState.posts];
+          let updatedTotalPosts = prevState.totalPosts;
           if (prevState.editPost) {
             const postIndex = prevState.posts.findIndex(
               (p) => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
           } else {
+            updatedTotalPosts++;
             if (prevState.posts.length >= 2) {
               updatedPosts.pop();
             }
@@ -289,6 +291,7 @@ class Feed extends Component {
             isEditing: false,
             editPost: null,
             editLoading: false,
+            totalPosts: updatedTotalPosts,
           };
         });
       })
